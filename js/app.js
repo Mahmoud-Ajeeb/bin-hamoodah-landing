ba61d62a79c31887da0f6e72761ce20d230f3a74
Mahmoud Ajeeb <mahmoud.ajeeb@outlook.com>
2020-07-20 07:47:19 +0400

    finished navbar

---
+++ js/app.js
@@ -0,0 +1,31 @@
+/* Author Mahmoud Ajeeb <mahmoud.ajeeb@outlook.com> */
+
+const navMobile = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+
+
+
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkShow 0.5s ease forwards ${index / 7 + .9}s`
+            }
+        })
+    });
+
+
+
+
+}
+
+navMobile();
+
+
